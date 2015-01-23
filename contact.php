2cbbe1b12f8ee90d5a933243fcf2a3f35dabbe16
FreeDaGeek <free.rosas@gmail.com>
2015-01-22 19:08:23 -0500

    demo add

--- contact.php
+++ contact.php
@@ -1,3 +1,36 @@
+  <?PHP
+/*
+    Contact Form from HTML Form Guide
+    This program is free software published under the
+    terms of the GNU Lesser General Public License.
+    See this page for more info:
+    http://www.html-form-guide.com/contact-form/php-contact-form-tutorial.html
+*/
+require_once("./include/fgcontactform.php");
+
+$formproc = new FGContactForm();
+
+
+//1. Add your email address here.
+//You can add more than one receipients.
+$formproc->AddRecipient('free.rosas@gmail.com'); //<<---Put your email address here
+
+
+//2. For better security. Get a random tring from this link: http://tinyurl.com/randstr
+// and put it here
+$formproc->SetFormRandomKey('Uh4ipNNG89tzVx6');
+
+
+if(isset($_POST['submitted']))
+{
+   if($formproc->ProcessForm())
+   {
+        $formproc->RedirectToURL("thank-you.php");
+   }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -22,10 +55,17 @@
     <!-- Custom styles for this template -->
     <link href="css/main.css" rel="stylesheet">
   </head>
-<!-- NAVBAR
+<!-- Contact Form
 ================================================== -->
-  <body>
-   <div class="container">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Contact us</title>
+      <link rel="STYLESHEET" type="text/css" href="contact.css" />
+      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+</head>
+<div class="container">
         <a href="index.html">
         <img id ="logo" src="images/logo.png" alt=""></a>
             <ul class="nav nav-pills">
@@ -33,20 +73,65 @@
               <li id="green"><a href="edusource.html">EduSource</a></li>
               <li id="purple"><a href="events.html">STEM Events</a></li>
               <li id="pink"><a href="diy.html">DIY Experiments</a></li>
-              <li id="green"><a href="contact.html">Contact Us</a></li>
+              <li id="green"><a href="contact.php">Contact Us</a></li>
             </ul>
+<body>
 
-<!-- Contact Form
-================================================== -->
-   Name: <input type="text" name="name" value="<?php echo $name;?>">
+<!-- Form Code Start -->
+<form id='contactus' action='<?php echo $formproc->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+<fieldset >
+<legend>Contact us</legend>
 
-   E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+<input type='hidden' name='<?php echo $formproc->GetFormIDInputName(); ?>' value='<?php echo $formproc->GetFormIDInputValue(); ?>'/>
+<input type='text'  class='spmhidip' name='<?php echo $formproc->GetSpamTrapInputName(); ?>' />
 
-   Website: <input type="text" name="website" value="<?php echo $website;?>">
+<div class='short_explanation'>* required fields</div>
 
-   Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+<div><span class='error'><?php echo $formproc->GetErrorMessage(); ?></span></div>
+<div class='container'>
+    <label for='name' >Your Full Name*: </label><br/>
+    <input type='text' name='name' id='name' value='<?php echo $formproc->SafeDisplay('name') ?>' maxlength="50" /><br/>
+    <span id='contactus_name_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <label for='email' >Email Address*:</label><br/>
+    <input type='text' name='email' id='email' value='<?php echo $formproc->SafeDisplay('email') ?>' maxlength="50" /><br/>
+    <span id='contactus_email_errorloc' class='error'></span>
+</div>
 
-   	</div>
+<div class='container'>
+    <label for='message' >Message:</label><br/>
+    <span id='contactus_message_errorloc' class='error'></span>
+    <textarea rows="10" cols="50" name='message' id='message'><?php echo $formproc->SafeDisplay('message') ?></textarea>
+</div>
+
+
+<div class='container'>
+    <input type='submit' name='Submit' value='Submit' />
+</div>
+
+</fieldset>
+</form>
+<!-- client-side Form Validations:
+Uses the excellent form validation script from JavaScript-coder.com-->
+
+<script type='text/javascript'>
+// <![CDATA[
+
+    var frmvalidator  = new Validator("contactus");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+    frmvalidator.addValidation("name","req","Please provide your name");
+
+    frmvalidator.addValidation("email","req","Please provide your email address");
+
+    frmvalidator.addValidation("email","email","Please provide a valid email address");
+
+    frmvalidator.addValidation("message","maxlen=2048","The message is too long!(more than 2KB!)");
+
+// ]]>
+</script>
 
      <!-- FOOTER -->
       <footer>
