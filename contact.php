b8210c5f41e1e7a5931e7cd43e26f8409dd71f0e
FreeDaGeek <free.rosas@gmail.com>
2015-01-22 21:54:21 -0500

    update via htdocs/php added

--- contact.php
+++ contact.php
@@ -1,3 +1,35 @@
+  <?PHP
+/*
+    Contact Form from HTML Form Guide
+    This program is free software published under the
+    terms of the GNU Lesser General Public License.
+    See this page for more info:
+    http://www.html-form-guide.com/contact-form/php-contact-form-tutorial.html
+*/
+require_once("./include/fgcontactform.php");
+
+$formproc = new FGContactForm();
+
+
+//1. Add your email address here.
+//You can add more than one receipients.
+$formproc->AddRecipient('free.rosas@gmail.com'); //<<---Put your email address here
+
+
+//2. For better security. Get a random tring from this link: http://tinyurl.com/randstr
+// and put it here
+$formproc->SetFormRandomKey('Uh4ipNNG89tzVx6');
+
+
+if(isset($_POST['submitted']))
+{
+   if($formproc->ProcessForm())
+   {
+        $formproc->RedirectToURL("thank-you.php");
+   }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,6 +41,9 @@
     <link rel="icon" href="../../favicon.ico">
 
     <title>Sister of STEM | Contact Us</title>
+
+    <!-- Font Awesome -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <!-- Latest compiled and minified CSS -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 
@@ -22,10 +57,17 @@
     <!-- Custom styles for this template -->
     <link href="css/main.css" rel="stylesheet">
   </head>
-<!-- NAVBAR
+<!-- Contact Form
 ================================================== -->
-  <body>
-   <div class="container">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Contact us</title>
+      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+</head>
+<div class="container">
+
         <a href="index.html">
         <img id ="logo" src="images/logo.png" alt=""></a>
             <ul class="nav nav-pills">
@@ -33,28 +75,74 @@
               <li id="green"><a href="edusource.html">EduSource</a></li>
               <li id="purple"><a href="events.html">STEM Events</a></li>
               <li id="pink"><a href="diy.html">DIY Experiments</a></li>
-              <li id="green"><a href="contact.html">Contact Us</a></li>
+              <li id="green"><a href="contact.php">Contact Us</a></li>
             </ul>
+<body>
+<div id="wrapper">
+<!-- Form Code Start -->
+<form id='contactus' action='<?php echo $formproc->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+<fieldset >
+<legend>Contact us</legend>
 
-<!-- Contact Form
-================================================== -->
-   Name: <input type="text" name="name" value="<?php echo $name;?>">
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+<input type='hidden' name='<?php echo $formproc->GetFormIDInputName(); ?>' value='<?php echo $formproc->GetFormIDInputValue(); ?>'/>
 
-   E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+<div><span class='error'><?php echo $formproc->GetErrorMessage(); ?></span></div>
+<div class='container'>
+    <!-- <label for='name' >Your Full Name: </label><br/> -->
+    <input type='text' name='name' placeholder="Full Name" id='name' value='<?php echo $formproc->SafeDisplay('name') ?>' maxlength="50" /><br/>
+    <span id='contactus_name_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <!--<label for='email' >Email Address:</label><br/> -->
+    <input type='text' name='email' id='email' placeholder="Email Address" value='<?php echo $formproc->SafeDisplay('email') ?>' maxlength="50" /><br/>
+    <span id='contactus_email_errorloc' class='error'></span>
+</div>
 
-   Website: <input type="text" name="website" value="<?php echo $website;?>">
+<div class='container'>
+    <!-- <label for='message' >Message:</label><br/> -->
+    <span id='contactus_message_errorloc' class='error'></span>
+    <textarea rows="10" cols="50" name='message' placeholder="Say Hello.." id='message'><?php echo $formproc->SafeDisplay('message') ?></textarea>
+</div>
 
-   Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
 
-   	</div>
+<div class='container'>
+    <input type='submit' id="button" name='Submit' value='Submit' />
+</div>
+
+</fieldset>
+</form>
+<!-- client-side Form Validations:
+Uses the excellent form validation script from JavaScript-coder.com-->
+
+<script type='text/javascript'>
+// <![CDATA[
+
+    var frmvalidator  = new Validator("contactus");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+    frmvalidator.addValidation("name","req","Please provide your name");
+
+    frmvalidator.addValidation("email","req","Please provide your email address");
+
+    frmvalidator.addValidation("email","email","Please provide a valid email address");
+
+    frmvalidator.addValidation("message","maxlen=2048","The message is too long!(more than 2KB!)");
+
+// ]]>
+</script>
+<div></div>
+</div>
 
      <!-- FOOTER -->
       <footer>
-      <i class="fa fa-twitter"></i>
-      <i class="fa fa-linkedin"></i>
-        <p class="pull-left">&copy; 2014 Pink Pineapple, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      <a href="www.facebook.com"class="fa fa-facebook fa-2x" id="sm"></a>
+      <a href="www.twitter.com" class="fa fa-twitter fa-2x" id="sm"></a>
+      <a href="www.instagram.com" class="fa fa-instagram fa-2x" id="sm"></a>
+      
+      <p class="pull-left">&copy; 2014 Pink Pineapple, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
       </footer>
-
+</div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
@@ -65,4 +153,5 @@
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
+
 </html>
