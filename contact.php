955c2c380fa2b06be2d31ae75e739fc1df45c96e
FreeDaGeek <free.rosas@gmail.com>
2015-01-26 10:50:04 -0500

    changes over the weekend

--- contact.php
+++ contact.php
@@ -1,39 +1,3 @@
-  <?PHP
-/*
-    Contact Form from HTML Form Guide
-    This program is free software published under the
-    terms of the GNU Lesser General Public License.
-    See this page for more info:
-    http://www.html-form-guide.com/contact-form/php-contact-form-tutorial.html
-*/
-require_once("./include/fgcontactform.php");
-
-$formproc = new FGContactForm();
-
-
-//1. Add your email address here.
-//You can add more than one receipients.
-$formproc->AddRecipient('free.rosas@gmail.com'); //<<---Put your email address here
-
-
-//2. For better security. Get a random tring from this link: http://tinyurl.com/randstr
-// and put it here
-$formproc->SetFormRandomKey('Uh4ipNNG89tzVx6');
-
-
-if(isset($_POST['submitted']))
-{
-   if($formproc->ProcessForm())
-   {
-        $formproc->RedirectToURL("thank-you.php");
-   }
-}
-
-?>
-<<<<<<< HEAD
-=======
-
->>>>>>> d5e28a2300fffa92b26a935aaaceddaeadffd236
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -57,28 +21,27 @@ if(isset($_POST['submitted']))
       <!-- Latest compiled and minified JavaScript -->
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
+      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
 
     <!-- Custom styles for this template -->
     <link href="css/main.css" rel="stylesheet">
-  </head>
-<!-- Contact Form
-================================================== -->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
-<head>
-      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
-      <title>Contact us</title>
-<<<<<<< HEAD
-      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
-</head>
-<div class="container">
+    <link rel="STYLESHEET" type="text/css" href="contact.css" />
+    <script type='text/javascript'>
 
-=======
-      <link rel="STYLESHEET" type="text/css" href="contact.css" />
-      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
-</head>
+
+    var frmvalidator  = new Validator("contact");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+    frmvalidator.addValidation("name","req","Please provide your name");
+
+    frmvalidator.addValidation("email","req","Please provide your email address");
+
+    frmvalidator.addValidation("email","email","Please provide a valid email address");
+
+    frmvalidator.addValidation("message","maxlen=2048","The message is too long!(more than 2KB!)");
+</script>
+
 <div class="container">
->>>>>>> d5e28a2300fffa92b26a935aaaceddaeadffd236
         <a href="index.html">
         <img id ="logo" src="images/logo.png" alt=""></a>
             <ul class="nav nav-pills">
@@ -89,77 +52,61 @@ if(isset($_POST['submitted']))
               <li id="green"><a href="contact.php">Contact Us</a></li>
             </ul>
 <body>
-<<<<<<< HEAD
 <div id="wrapper">
-<!-- Form Code Start -->
-<form id='contactus' action='<?php echo $formproc->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
-<fieldset >
-<legend>Contact us</legend>
+      <!-- Form Code Start -->
+      <form id='contactus' action='<?php echo $formproc->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+  <fieldset>
+      <legend>Contact us</legend>
+        <input type='hidden' name='submitted' id='submitted' value='1'/>
+        <input type='hidden' name='<?php echo $formproc->GetFormIDInputName(); ?>' value='<?php echo $formproc->GetFormIDInputValue(); ?>'/>
+        <div>
+          <span class='error'><?php echo $formproc->GetErrorMessage(); ?></span>
+        </div>
+        <div class='container'>
+          <input type='text' name='name' id='name' placeholder="Full Name" value='<?php echo $formproc->SafeDisplay('name') ?>' maxlength="50" /><br/>
+          <span id='contactus_name_errorloc' class='error'></span>
+        </div>
 
-<input type='hidden' name='submitted' id='submitted' value='1'/>
-<input type='hidden' name='<?php echo $formproc->GetFormIDInputName(); ?>' value='<?php echo $formproc->GetFormIDInputValue(); ?>'/>
+        <div class='container'>
+            <input type='text' name='email' id='email' placeholder='Email' value='<?php echo $formproc->SafeDisplay('email') ?>' maxlength="50" /><br/>
+            <span id='contactus_email_errorloc' class='error'></span>
+        </div>
 
-<div><span class='error'><?php echo $formproc->GetErrorMessage(); ?></span></div>
-<div class='container'>
-    <!-- <label for='name' >Your Full Name: </label><br/> -->
-    <input type='text' name='name' placeholder="Full Name" id='name' value='<?php echo $formproc->SafeDisplay('name') ?>' maxlength="50" /><br/>
-    <span id='contactus_name_errorloc' class='error'></span>
-</div>
-<div class='container'>
-    <!--<label for='email' >Email Address:</label><br/> -->
-    <input type='text' name='email' id='email' placeholder="Email Address" value='<?php echo $formproc->SafeDisplay('email') ?>' maxlength="50" /><br/>
-=======
-
-<!-- Form Code Start -->
-<form id='contactus' action='<?php echo $formproc->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
-<fieldset >
-<legend>Contact us</legend>
-
-<input type='hidden' name='submitted' id='submitted' value='1'/>
-<input type='hidden' name='<?php echo $formproc->GetFormIDInputName(); ?>' value='<?php echo $formproc->GetFormIDInputValue(); ?>'/>
-<input type='text'  class='spmhidip' name='<?php echo $formproc->GetSpamTrapInputName(); ?>' />
-
-<div class='short_explanation'>* required fields</div>
-
-<div><span class='error'><?php echo $formproc->GetErrorMessage(); ?></span></div>
-<div class='container'>
-    <label for='name' >Your Full Name*: </label><br/>
-    <input type='text' name='name' id='name' value='<?php echo $formproc->SafeDisplay('name') ?>' maxlength="50" /><br/>
-    <span id='contactus_name_errorloc' class='error'></span>
-</div>
-<div class='container'>
-    <label for='email' >Email Address*:</label><br/>
-    <input type='text' name='email' id='email' value='<?php echo $formproc->SafeDisplay('email') ?>' maxlength="50" /><br/>
->>>>>>> d5e28a2300fffa92b26a935aaaceddaeadffd236
-    <span id='contactus_email_errorloc' class='error'></span>
-</div>
-
-<div class='container'>
-<<<<<<< HEAD
-    <!-- <label for='message' >Message:</label><br/> -->
-    <span id='contactus_message_errorloc' class='error'></span>
-    <textarea rows="10" cols="50" name='message' placeholder="Say Hello.." id='message'><?php echo $formproc->SafeDisplay('message') ?></textarea>
-=======
-    <label for='message' >Message:</label><br/>
-    <span id='contactus_message_errorloc' class='error'></span>
-    <textarea rows="10" cols="50" name='message' id='message'><?php echo $formproc->SafeDisplay('message') ?></textarea>
->>>>>>> d5e28a2300fffa92b26a935aaaceddaeadffd236
-</div>
-
-
-<div class='container'>
-<<<<<<< HEAD
-    <input type='submit' id="button" name='Submit' value='Submit' />
-</div>
-
-</fieldset>
+        <div class='container'>
+            <span id='contactus_message_errorloc' class='error'></span>
+            <textarea rows="10" cols="50" name='message' placeholder="Say Hello.." id='message'><?php echo $formproc->SafeDisplay('message') ?></textarea>
+        </div>
+        <div class='container'>
+            <input type='submit' id="button" name='Submit' value='Submit' />
+        </div>
+        <div>
+        </div>
+    </fieldset>
 </form>
+</div>
+<!--
+      <div class="container">
+          <div class="row-fluid">
+              <div id="image-contact">
+                <div class="row-fluid">
+                   <div class="span3">
+                        <img src="images/image003.jpg">
+                   </div>
+                   <div class="span3">
+                         <img src="images/image003.jpg">
+                    </div>     
+                   <div class="span3">
+                         <img src="images/image003.jpg">
+                    </div> 
+                </div>
+                </div>
+             </div>
+          </div>
+      </div>-->
+
 <!-- client-side Form Validations:
 Uses the excellent form validation script from JavaScript-coder.com-->
-
 <script type='text/javascript'>
-// <![CDATA[
-
     var frmvalidator  = new Validator("contactus");
     frmvalidator.EnableOnPageErrorDisplay();
     frmvalidator.EnableMsgsTogether();
@@ -170,44 +117,14 @@ Uses the excellent form validation script from JavaScript-coder.com-->
     frmvalidator.addValidation("email","email","Please provide a valid email address");
 
     frmvalidator.addValidation("message","maxlen=2048","The message is too long!(more than 2KB!)");
-
-// ]]>
 </script>
-<div></div>
-</div>
-=======
-    <input type='submit' name='Submit' value='Submit' />
-</div>
 
-</fieldset>
-</form>
-<!-- client-side Form Validations:
-Uses the excellent form validation script from JavaScript-coder.com-->
-
-<script type='text/javascript'>
-// <![CDATA[
-
-    var frmvalidator  = new Validator("contactus");
-    frmvalidator.EnableOnPageErrorDisplay();
-    frmvalidator.EnableMsgsTogether();
-    frmvalidator.addValidation("name","req","Please provide your name");
-
-    frmvalidator.addValidation("email","req","Please provide your email address");
-
-    frmvalidator.addValidation("email","email","Please provide a valid email address");
-
-    frmvalidator.addValidation("message","maxlen=2048","The message is too long!(more than 2KB!)");
-
-// ]]>
-</script>
->>>>>>> d5e28a2300fffa92b26a935aaaceddaeadffd236
 
      <!-- FOOTER -->
       <footer>
       <a href="www.facebook.com"class="fa fa-facebook fa-2x" id="sm"></a>
       <a href="www.twitter.com" class="fa fa-twitter fa-2x" id="sm"></a>
       <a href="www.instagram.com" class="fa fa-instagram fa-2x" id="sm"></a>
-      
       <p class="pull-left">&copy; 2014 Pink Pineapple, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
       </footer>
 </div>
@@ -221,5 +138,4 @@ Uses the excellent form validation script from JavaScript-coder.com-->
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
-
 </html>
